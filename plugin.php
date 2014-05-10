865c742a76598f463de1cdc2f81c436c2485c28f
Toby Zerner <toby.zerner@gmail.com>
2014-05-10 13:34:33 +0930

    require esoTalk 1.0.0g4

--- plugin.php
+++ plugin.php
@@ -11,7 +11,10 @@ ET::$pluginInfo["GitHubLinks"] = array(
 	"author" => "esoTalk Team",
 	"authorEmail" => "support@esotalk.org",
 	"authorURL" => "http://esotalk.org",
-	"license" => "GPLv2"
+	"license" => "GPLv2",
+	"dependencies" => array(
+		"esoTalk" => "1.0.0g4"
+	)
 );
 
 
